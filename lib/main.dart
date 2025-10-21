d89cb27e85a8ba4d3906b09c976ed34ec19a1c8e
Adham Hawash <Adhamhawash@gmail.com>
2025-10-22 02:17:15 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:evently/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(evntleApp());
+}
+
+
+class evntleApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routeName: (_) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
