c2fab9d201e7179f9637c1d23162f48a51618bd1
Adham Hawash <Adhamhawash@gmail.com>
2025-12-05 16:42:22 +0200

    intgration firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ import 'package:evently/auth/login_screen.dart';
 import 'package:evently/on_Bording_screen.dart';
 import 'package:evently/auth/regester_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); 
   runApp(evntleApp());
 }
 
